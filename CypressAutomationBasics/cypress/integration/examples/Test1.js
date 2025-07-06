1bfc776fde976da23a23e7988950b8c850bca8fe
Kamalnath <kamalnath121998@gmail.vom>
2025-07-06 23:46:03 +0530

    Cypress First commit

---
+++ CypressAutomationBasics/cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+//cypress code - sopec file
+describe('My First Test Suite', () => {
+  it('My First Test Case', () => {
+   // expect(true).to.equal(true)
+   cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  })
+})
+
