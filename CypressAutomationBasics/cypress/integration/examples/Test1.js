b0cc2b447c7e716ec819055b71704e0db5f2201d
Kamalnath <kamalnath121998@gmail.vom>
2025-07-07 14:47:00 +0530

    Cypress automation using locators

--- CypressAutomationBasics/cypress/integration/examples/Test1.js
+++ CypressAutomationBasics/cypress/integration/examples/Test1.js
@@ -1,8 +1,12 @@
 //cypress code - sopec file
+// <reference types="Cypress"/>
 describe('My First Test Suite', () => {
   it('My First Test Case', () => {
    // expect(true).to.equal(true)
    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+   cy.get('.search-keyword').type('ca');
+   cy.wait(2000);
+   cy.get('.product:visible').should('have.length', 4);
   })
 })
 
