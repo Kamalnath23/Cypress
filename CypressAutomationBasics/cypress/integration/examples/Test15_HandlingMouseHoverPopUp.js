efeddc151d2d86b7eb2a964ef7bc2c7d9fed431e
Kamalnath <kamalnath121998@gmail.vom>
2025-10-07 11:06:16 +0530

    Handling mouse hover pop up

---
+++ CypressAutomationBasics/cypress/integration/examples/Test15_HandlingMouseHoverPopUp.js
@@ -0,0 +1,14 @@
+describe('Handling mouse hover popup', ()=>{
+    it('Test mouse hover pop up', ()=>{
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        //cy.get('.mouse-hover-content').invoke('show')  //to show the hidden element
+        cy.get('#mousehover').invoke('show');
+        cy.contains('Top').click({force: true}); //force:true is used to click on hidden element
+        cy.url().should('include', 'top');
+        cy.go('back');
+        cy.get('#mousehover').invoke('show');
+        cy.contains('Reload').click({force: true});
+        cy.url().should('include', 'AutomationPractice');
+    });
+
+})
